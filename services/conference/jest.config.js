34980fdf6571b5a79449d26ced063a8415a57900
Guo Zile <zlguo1996@gmail.com>
2020-05-06 17:49:59 +0900

    add babel

---
+++ services/conference/jest.config.js
@@ -0,0 +1,27 @@
+// jest.config.js
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/libs"
+  ],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: `<rootDir>/${compilerOptions.baseUrl}`
+  }),
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
+  }
+}
