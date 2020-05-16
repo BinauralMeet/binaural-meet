070d5717c4fb67de3f1cf637f58ddf22e366f443
Haoyan.Li <haoyan.li.ac@gmail.com>
2020-05-17 02:49:58 +0900

    feat: add dummy visualizer for storybook

--- services/conference/src/scripts/components/__stories__/API/Dummy.stories.tsx
+++ services/conference/src/scripts/components/__stories__/API/Dummy.stories.tsx
@@ -1,7 +1,13 @@
 import {dummyConnection as connection, ConnectionStates, Logger} from '@models/api'
 import {dummyConnectionStore as store, StoreProvider, useStore} from '@test-utils/DummyParticipants'
 import {useObserver} from 'mobx-react-lite'
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from '@material-ui/core/CardHeader'
+import Grid from "@material-ui/core/Grid";
+import { Button, ButtonGroup, TableRow, TableContainer, Table, TableBody } from '@material-ui/core'
+
 
 export default {
   title: 'Dummy Connection',
@@ -75,10 +81,64 @@ const LocalVideo: React.FC<{}> = () => {
   return displayEl
 }
 
+const DummyParticipantVisualizer: React.FC<{}> = () => {
+  const [participants, setParticipants] = useState<number[]>([])
+  const handleAddOnClick = () => {
+    const currParticipants = [...participants]
+    currParticipants.push(currParticipants.length + 1)
+    setParticipants(currParticipants)
+  }
+  const handleDeleteOnClick = () => {
+    const currParticipants = [...participants]
+    currParticipants.pop()
+    setParticipants(currParticipants)
+  }
+  const controls = (
+    <div id="controls_container">
+      <ButtonGroup>
+        <Button onClick={handleAddOnClick}>Add participant</Button>
+        <Button onClick={handleDeleteOnClick}>Delete last participant</Button>
+      </ButtonGroup>
+    </div>
+  )
+  const elements = participants.map(
+    (value: number) => {
+      return (
+        <Card key={value}>
+          <CardHeader title={`Pariticipant ${value}`}>
+          </CardHeader>
+          <CardContent>
+            <p>Just a line content for placement.</p>
+          </CardContent>
+        </Card>
+      )
+    },
+  )
+
+  // useEffect(
+  //   () => {
+
+  //   },
+  //   [],
+  // )
+
+  return (
+    <div>
+      {controls}
+      <Grid container={true} spacing={3}>
+        {elements}
+      </Grid>
+    </div>
+  )
+}
+
 export const DummyConnection: React.FC<{}> = () => {
   return (
-    <StoreProvider value={store}>
-      <LocalVideo />
-    </StoreProvider>
+    <div>
+      <DummyParticipantVisualizer />
+    </div>
+    // <StoreProvider value={store}>
+    //   <LocalVideo />
+    // </StoreProvider>
   )
 }
