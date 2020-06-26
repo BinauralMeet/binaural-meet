b972cc14d8a4044329b45f6e4807ae84d821a732
Guo Zile <zlguo1996@gmail.com>
2020-06-26 11:23:03 +0900

    Create index.ts

---
+++ services/conference/src/scripts/components/map/LocalLayer/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Layer for displaying windows customized by local participant.
+ * Contents in this layer would only be shown in local particiant's interface.
+ * (e.g. participant settings, pinned videos)
+ */
