95d06dfbc8e481b5c0b3eadd71a04d3fea76ee8c
Guo Zile <zlguo1996@gmail.com>
2020-07-04 11:29:10 +0900

    fix stream avatar

--- services/conference/src/scripts/models/TrackMiddleware/ChooseDevice.ts
+++ services/conference/src/scripts/models/TrackMiddleware/ChooseDevice.ts
@@ -1,41 +1,31 @@
-import {reaction} from 'mobx'
 import {connection} from '@models/api'
 import participants from '@stores/participants/Participants'
-import JitsiMeetJS, {JitsiValues, JitsiLocalTrack, JitsiTrackOptions} from 'lib-jitsi-meet'
+import JitsiMeetJS, {JitsiLocalTrack, JitsiTrackOptions, JitsiValues} from 'lib-jitsi-meet'
+import {reaction} from 'mobx'
 
-function replaceTrack(newTrack:JitsiLocalTrack){
+function replaceTrack(newTrack:JitsiLocalTrack) {
   const oldTracks = connection.conference?.getLocalTracks(newTrack.getType())
-  if (oldTracks !== undefined){
+  if (oldTracks !== undefined) {
     connection.conference?.replaceTrack(oldTracks[0], newTrack)
   }
 }
 
-reaction(() => participants.local.get().plugins.streamControl.audioInputDevice,
+reaction(
+  () => participants.local.get().plugins.streamControl.audioInputDevice,
   (did) => {
-    JitsiMeetJS.createLocalTracks({ devices:['audio'], micDeviceId: did }).then(
-      (tracks: JitsiLocalTrack[]) =>{ replaceTrack(tracks[0]) }
+    JitsiMeetJS.createLocalTracks({devices:['audio'], micDeviceId: did}).then(
+      (tracks: JitsiLocalTrack[]) => { replaceTrack(tracks[0]) },
     )
-  }
+  },
 )
 
-reaction(() => participants.local.get().plugins.streamControl.videoInputDevice,
+reaction(
+  () => participants.local.get().plugins.streamControl.videoInputDevice,
   (did) => {
-    JitsiMeetJS.createLocalTracks({ devices:['video'], cameraDeviceId: did }).then(
-      (tracks: JitsiLocalTrack[]) =>{
+    JitsiMeetJS.createLocalTracks({devices:['video'], cameraDeviceId: did}).then(
+      (tracks: JitsiLocalTrack[]) => {
         replaceTrack(tracks[0])
-
-//  This does not work at all
-//  participants.local.get().stream.avatarStream = new MediaStream([tracks[0].getTrack()])
-
-      //  This works first time.
-        participants.local.get().stream.avatarStream?.getTracks().forEach( (tr) => {
-          console.log("RemoveTrack", tr)
-          participants.local.get().stream.avatarStream?.removeTrack(tr)
-        })
-        participants.local.get().stream.avatarStream?.addTrack(tracks[0].getTrack())
-        console.log("AddTrack", tracks[0].getTrack())
-        console.log("Result", participants.local.get().stream.avatarStream?.getTracks())
-      }
+      },
     )
-  }
+  },
 )
