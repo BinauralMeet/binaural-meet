26c68e63b45e2c60f89e06f140afddef4647f64e
Guo Zile <zlguo1996@gmail.com>
2020-07-07 15:34:49 +0900

    add store for switch stereo audio

--- services/conference/src/scripts/stores/participants/LocalParticipant.ts
+++ services/conference/src/scripts/stores/participants/LocalParticipant.ts
@@ -1,8 +1,11 @@
+import {observable} from 'mobx'
 import {DevicePreference} from './localPlugins'
 import {Participant} from './Participant'
 
 export class LocalParticipant extends Participant {
   devicePreference = new DevicePreference()
+
+  @observable useStereoAudio = true
 
   constructor(id: string) {
     super(id)
