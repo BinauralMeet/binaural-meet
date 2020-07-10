055c4576ae526f2762e5e9d2be3cac241495d5ad
Guo Zile <zlguo1996@gmail.com>
2020-07-10 17:03:21 +0900

    connect url parameter audio to store

---
+++ services/conference/src/scripts/models/initStores/localParticipant.ts
@@ -0,0 +1,14 @@
+import {urlParameters} from '@models/url'
+import {participantsStore} from '@stores/participants'
+import {LocalParticipant} from '@stores/participants/LocalParticipant'
+import {reaction} from 'mobx'
+
+function applyUrlParameters(local: LocalParticipant) {
+  local.useStereoAudio = urlParameters.audio === 'mono' ? false : true
+  console.log('audio parameter', urlParameters.audio)
+}
+
+reaction(
+  () => participantsStore.local.get(),
+  applyUrlParameters,
+)
--- services/conference/src/scripts/models/utils/index.ts
+++ services/conference/src/scripts/models/utils/index.ts
@@ -1,4 +1,3 @@
 export * from './utils'
 export * from './coordinates'
 export * from './promise'
-export * from './url'
