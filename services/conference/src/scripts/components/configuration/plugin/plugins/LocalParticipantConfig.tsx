f04edfd9cf5e35f2858959e044f00037f0b5f95e
Shoichi Hasegawa <hasevr@gmail.com>
2020-07-24 21:05:27 +0900

    edit name and email

--- services/conference/src/scripts/components/configuration/plugin/plugins/LocalParticipantConfig.tsx
+++ services/conference/src/scripts/components/configuration/plugin/plugins/LocalParticipantConfig.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import {useStore} from '@hooks/ParticipantsStore'
+import {Information} from '@models/Participant'
+import React, {useState} from 'react'
 import {PluginBase} from '../PluginBase'
 import {registerPlugin} from '../registery'
 
@@ -8,10 +10,32 @@ interface Props {
   id: string
 }
 
+
+function useInput<T>(initialValue:T) {
+  const [value, set] = useState(initialValue)
+
+  function handler(e:React.ChangeEvent<HTMLInputElement>) {
+    (set as any)(e.target.value)
+  }
+
+  return {value, onChange: handler}
+}
+
 const LocalParticipantConfig: React.FC<Props> = (props: Props) => {
-  return <div>
-    {'LocalParticipantConfig,LocalParticipantConfig,LocalParticipantConfig,LocalParticipantConfig,LocalParticipantConfig'}
-  </div>
+  const participants = useStore()
+  const local = participants.local.get()
+  const name = useInput(local.information.name)
+  const email = useInput(local.information.email)
+  function handleSubmit(ev: React.FormEvent) {
+    local.information.name = name.value
+    ev.preventDefault()
+  }
+
+  return <form onSubmit={handleSubmit}>
+    Name: <input type="text" {...name} /> <br />
+    Email: <input type="text" {...email} /> <br />
+    <input type="submit" value=" Submit " />
+  </form>
 }
 
 
