a60e572f6d86a47e7666011489827b5760f8981a
Shoichi Hasegawa <hasevr@gmail.com>
2020-07-31 19:20:25 +0900

    Add rolloff setting to footer.

--- services/conference/src/scripts/stores/AudioParameters/StereoParameters.ts
+++ services/conference/src/scripts/stores/AudioParameters/StereoParameters.ts
@@ -1,5 +1,10 @@
+import {PARTICIPANT_SIZE} from '@models/Participant'
 import {pick} from 'lodash'
-import {action, observable} from 'mobx'
+import {action, computed, observable} from 'mobx'
+
+const PERCENT = 100
+const ROLLOFFSCALE = 3  //  range 0 corresponds: 1/(2*rollOffScale) for 2avatars, 1/(5*rollOffScale) for 5avatars
+const DEFAULT_HEARABLE_RANGE = 40
 
 // Doc of panner node parameters: https://developer.mozilla.org/en-US/docs/Web/API/PannerNode
 export class StereoParameters implements ConfigurableParams {
@@ -9,8 +14,18 @@ export class StereoParameters implements ConfigurableParams {
   @observable distanceModel: DistanceModelType = 'inverse'
   @observable maxDistance = 10000
   @observable panningModel: PanningModelType = 'HRTF'
-  @observable refDistance = 10
-  @observable rolloffFactor = 1
+  @observable refDistance = PARTICIPANT_SIZE
+  @observable rolloffFactor = (PERCENT - DEFAULT_HEARABLE_RANGE) * (this.refDistance * ROLLOFFSCALE / PERCENT)
+
+  //  0 to 100, 0 has strongest attenuation
+  @computed
+  get hearableRange() {
+    return PERCENT - this.rolloffFactor / (this.refDistance * ROLLOFFSCALE / PERCENT)
+  }
+  @action
+  setHearableRange(range:number) {
+    this.rolloffFactor = (PERCENT - range) * (this.refDistance * ROLLOFFSCALE / PERCENT)
+  }
 
   @action
   updateParameters(params: Partial<ConfigurableParams>) {
