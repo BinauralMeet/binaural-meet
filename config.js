52cb7efdc6442731f526cda281f508a541d76e5b
Shoichi Hasegawa <hasevr@gmail.com>
2020-09-12 21:11:22 +0900

    Updates

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ const rtcConfig = {
     video:{
       facingMode:'user',
       width:{
-        max:1280,
+        max:1920,
         min:160,
         ideal:320,
       },
       height:{
-        max:720,
+        max:1080,
         min:120,
         ideal:240,
       },
@@ -39,6 +39,23 @@ const config_haselab_net = {
   rtc:rtcConfig
 }
 
+const config_party_haselab = {
+  hosts: {
+    domain: 'meet.jitsi',
+    muc: 'muc.meet.jitsi',
+    focus: 'focus.meet.jitsi',
+  },
+//  openBridgeChannel: 'datachannel', // One of true, 'datachannel', or 'websocket'
+  bosh: 'https://party.haselab.net/http-bind', // FIXME: use xep-0156 for that
+  clientNode: 'http://jitsi.org/jitsimeet',
+  focusUserJid: 'focus@auth.meet.jitsi',
+  channelLastN: -1,
+  p2p: {
+    enabled: false,
+  },
+  rtc:rtcConfig
+}
+
 const config_alpha = {
   hosts: {
     domain: 'alpha.jitsi.net',
@@ -52,6 +69,23 @@ const config_alpha = {
     useStunTurn: true, // use XEP-0215 to fetch STUN and TURN servers for the P2P connection
   },
   bosh: 'https://alpha.jitsi.net/http-bind', // FIXME: use xep-0156 for that
+  rtc:rtcConfig
+}
+
+const config_local = {
+  hosts: {
+    domain: 'meet.jitsi',
+    muc: 'muc.meet.jitsi',
+    focus: 'focus.meet.jitsi',
+  },
+//  openBridgeChannel: 'datachannel', // One of true, 'datachannel', or 'websocket'
+  bosh: 'https://localhost:8443/http-bind', // FIXME: use xep-0156 for that
+  clientNode: 'http://jitsi.org/jitsimeet',
+  focusUserJid: 'focus@auth.meet.jitsi',
+  channelLastN: -1,
+  p2p: {
+    enabled: false,
+  },
   rtc:rtcConfig
 }
 
