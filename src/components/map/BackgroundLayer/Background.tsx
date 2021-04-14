2dc2893ca4c1835ba1e18812165bca18a8493229
Shoichi Hasegawa <hasevr@gmail.com>
2021-04-14 17:14:52 +0900

    Fix: Remove Background.tsx completely when screen shared in background.

--- src/components/map/BackgroundLayer/Background.tsx
+++ src/components/map/BackgroundLayer/Background.tsx
@@ -7,15 +7,18 @@ const HALF = 0.5
 const useStyles = makeStyles({
   img: (props:BackgroundProps) => {
     if (props.transparent) {
+      return {display: 'none'}
+      /*
       return  {
         position: 'absolute',
         top: - MAP_SIZE * HALF,
         left: - MAP_SIZE * HALF,
         backgroundColor: 'rgba(0,0,0,0.1)',
+        // tslint:disable-next-line: max-line-length
         backgroundImage: `url("data:image/svg+xml,%3Csvg width='80' height='80' viewBox='0 0 80 80' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='rgba(255,255,255,0.1)' fill-opacity='0.4'%3E%3Cpath d='M50 50c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10s-10-4.477-10-10 4.477-10 10-10zM10 10c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10S0 25.523 0 20s4.477-10 10-10zm10 8c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm40 40c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
         height: MAP_SIZE,
         width: MAP_SIZE,
-      }
+      }*/
     }
 
     return {
