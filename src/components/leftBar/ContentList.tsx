d6d376d6ce288af82933fd305aab95d1810fb55a
Shoichi Hasegawa <hasevr@gmail.com>
2021-10-22 18:15:31 +0900

    Fix: textTransform:none for ContentList.tsx

--- src/components/leftBar/ContentList.tsx
+++ src/components/leftBar/ContentList.tsx
@@ -42,8 +42,8 @@ export const ContentLine: React.FC<BMProps & TextLineStyle &
 
     return <>
       <Tooltip title={<>{props.content.name}<br />{props.content.ownerName}</>} placement="right">
-        <Button variant="contained" className={classes.line}
-          style={{backgroundColor:colors[0], color:colors[1], margin: '1px 0 1px 0'}} ref={ref}
+        <Button ref={ref} variant="contained" className={classes.line}
+          style={{backgroundColor:colors[0], color:colors[1], margin: '1px 0 1px 0', textTransform:'none'}}
           onClick={() => {
             const found = contents.find(props.content.id)
             if (found){
