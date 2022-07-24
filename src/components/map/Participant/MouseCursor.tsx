80ce0b6b1e80084f389429d2fdeca10f1b5efd70
Shoichi Hasegawa <hasevr@gmail.com>
2022-07-24 20:04:01 +0900

    Fix: Mouse cursor's zIndex.

--- src/components/map/Participant/MouseCursor.tsx
+++ src/components/map/Participant/MouseCursor.tsx
@@ -21,7 +21,7 @@ export const MouseCursor: React.FC<MouseCursorProps> = (props:MouseCursorProps) 
   const isLocal = props.participantId === participants.localId
 
   const cursor = <div style={{width:18, height:30, left:position[0], top:position[1], position:'absolute',
-    pointerEvents: isLocal ? 'none' : 'auto',
+    pointerEvents: isLocal ? 'none' : 'auto', zIndex:isLocal ? 6000 : participant.zIndex+4000
   }}>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 100">
       <polygon points="32,100 53,92 36,57 60,56 0,0 0,81 16,65 32,100" stroke="black" fill={color} strokeWidth="6" />
