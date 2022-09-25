0eb247329d100514408a050b64d7849d7360becc
Shoichi Hasegawa <hasevr@gmail.com>
2022-09-25 11:04:10 +0900

    Fix: forgot to commit.

---
+++ src/models/conference/ConferenceLog.ts
@@ -0,0 +1,10 @@
+export const CONNECTIONLOG = false
+export const TRACKLOG = false        // show add, remove... of tracks
+export const EVENTLOG = false
+export const SENDLOG = false
+
+export const trackLog = TRACKLOG ? console.log : (a:any) => {}
+export const connLog = CONNECTIONLOG ? console.log : (a:any) => {}
+export const connDebug = CONNECTIONLOG ? console.debug : (a:any) => {}
+export const eventLog = EVENTLOG ? console.log : (a:any) => {}
+export const sendLog = SENDLOG ? console.log : (a:any) => {}
