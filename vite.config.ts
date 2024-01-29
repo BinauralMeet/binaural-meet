b6c61cba377fd4c397217acdf8211fa21707ef0d
wfy1998 <wang.f.ac@m.titech.ac.jp>
2024-01-29 14:15:58 +0900

    delete CRA files

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,6 @@ const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
   build: {
     outDir: "build",
   },
